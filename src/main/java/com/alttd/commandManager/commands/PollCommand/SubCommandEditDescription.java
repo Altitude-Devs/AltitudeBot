9642a80f085ead4c66a7261f6c873f09deac0d3a
Stijn <stijn.d2000@hotmail.com>
2022-04-08 21:57:55 +0200

    Start for editing descriptions for polls

--- src/main/java/com/alttd/commandManager/commands/PollCommand/SubCommandEditDescription.java
+++ src/main/java/com/alttd/commandManager/commands/PollCommand/SubCommandEditDescription.java
@@ -3,8 +3,12 @@ package com.alttd.commandManager.commands.PollCommand;
 import com.alttd.commandManager.DiscordCommand;
 import com.alttd.commandManager.SubCommand;
 import com.alttd.util.OptionMappingParsing;
+import com.alttd.util.Util;
+import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.GuildMessageChannel;
+import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.InteractionHook;
 
 public class SubCommandEditDescription extends SubCommand {
     protected SubCommandEditDescription(DiscordCommand parent) {
@@ -27,6 +31,17 @@ public class SubCommandEditDescription extends SubCommand {
         String description = OptionMappingParsing.getString("description", event, getName());
         if (description == null)
             return;
+    }
+
+    //Copied over while working on add button
+    private void updatePoll(GuildMessageChannel channel, int rowId, String buttonName, Message message,
+                            InteractionHook hook) {
+        EmbedBuilder firstEmbedBuilder = Util.getFirstEmbedBuilder(message);
+        if (firstEmbedBuilder == null) {
+            hook.editOriginalEmbeds(Util.genericErrorEmbed("Error", "Unable to get embed from poll message."))
+                    .queue();
+        }
+        message.editMessageEmbeds(firstEmbedBuilder.build());//TODO finish this
     }
 
     @Override
