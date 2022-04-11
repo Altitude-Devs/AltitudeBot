c3d2b215d5976479174b5d27ec9fa331897fb32b
Teriuihi <teriuihi@alttd.com>
2022-04-11 02:42:11 +0200

    Attempted to add mysql

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,6 +8,8 @@ dependencyResolutionManagement {
         maven("https://m2.dv8tion.net/releases/")
         // Configurate
         maven("https://repo.spongepowered.org/maven")
+        // MySQL
+        maven("https://jcenter.bintray.com")
     }
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
 }
